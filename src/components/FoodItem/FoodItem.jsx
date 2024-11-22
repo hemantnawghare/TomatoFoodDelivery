b99b4545c44ace9ac0cb8fb909ec27ffab290cdb
Hemant <hemantnawghare13@gmail.com>
2024-11-22 15:07:01 +0530

    new modification

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -5,12 +5,12 @@ import { StoreContext } from '../../context/StoreConetxt'
 
 const FoodItem = ({id,name,price,description,image}) => {
 
-    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
+    const {cartItems,addToCart,removeFromCart, url} = useContext(StoreContext)
 
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
-            <img src={image} alt="" className="food-item-image" />
+            <img src={url+"/images/"+image} alt="" className="food-item-image" />
             {
                 !cartItems[id]
                 ? <img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
@@ -29,7 +29,7 @@ const FoodItem = ({id,name,price,description,image}) => {
             <p className="food-item-desc">
                 {description}
             </p>
-            <p className="food-item-price">Rs {price*10}</p>
+            <p className="food-item-price">Rs {price}</p>
         </div>
     </div>
   )
--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,22 +1,59 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreConetxt'
+import axios from 'axios'
 
 const LoginPopup = ({setShowLogin}) => {
+
+    const {url,setToken} = useContext(StoreContext)
+
     const [currState,setCurrState] = useState("Sign Up")
+    const [data,setData] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value;
+        setData(data =>({...data,[name]:value}))
+    }
+
+    const onLogin = async (event) => {
+        event.preventDefault();
+        let newUrl = url;
+        if(currState=="Login") {
+            newUrl += "/api/user/login"
+        } else {
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl,data);
+        if(response.data.success) {
+            setToken(response.data.token)
+            localStorage.setItem("token",response.data.token)
+            setShowLogin(false)
+        }
+        else {
+            alert(response.data.message)
+        }
+    }
+
   return (
     <div className='login-popup'>
-        <form action="" className="login-popup-container">
+        <form onSubmit={onLogin} className="login-popup-container">
             <div className="login-popup-title">
                 <h2>{currState}</h2>
                 <img onClick={()=>setShowLogin=setShowLogin(false)} src={assets.cross_icon} />
             </div>
             <div className="login-popup-inputs">
-                {currState==="Login"?<></>:<input type="text" placeholder="Your Name" required/>}
-                <input type="email" placeholder="Your Email" required />
-                <input type="password" placeholder='Password' required/>
+                {currState==="Login"?<></>:<input type="text" name="name" onChange={onChangeHandler} value={data.name} placeholder="Your Name" required/>}
+                <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder="Your Email" required />
+                <input name="password" onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required/>
             </div>
-            <button>{currState==="Sign Up"?'Create Account':'Login'}</button>
+            <button type="submit">{currState==="Sign Up"?'Create Account':'Login'}</button>
             <div className="login-popup-condition">
                 <input type="checkbox" required/>
                 <p>By Continuing, i agree to the terms of use & privacy policy.</p>
